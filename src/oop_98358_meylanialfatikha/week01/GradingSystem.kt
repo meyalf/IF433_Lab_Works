97323628cb9af73378859fda7b31a06bddcd12a9
meyalf <meylanialfatikha@gmail.com>
2026-02-07 10:15:43 +0700

    week01: initial setup and procedural code

---
+++ src/oop_98358_meylanialfatikha/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_98358_meylanialfatikha.week01
+
+fun main(args: Array<String>) {
+    var studentName: String = "Alice"
+    var score: Int = 85
+
+    println("Student: " + studentName)
+    println("Score: " + score)
+}
