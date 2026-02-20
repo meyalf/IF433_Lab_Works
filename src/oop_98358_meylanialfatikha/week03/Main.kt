a9cacdc9f7a4991e30957d0dcab16850c12bdf0b
meyalf <meylanialfatikha@gmail.com>
2026-02-20 15:11:23 +0700

    "week03: (task 2) implement Player testing in main"

--- src/oop_98358_meylanialfatikha/week03/Main.kt
+++ src/oop_98358_meylanialfatikha/week03/Main.kt
@@ -1,7 +1,6 @@
 package oop_98358_meylanialfatikha.week03
 
 fun main() {
-    // ========== TEST EMPLOYEE ==========
     val e = Employee("Budi")
 
     e.salary = -1000
@@ -15,20 +14,35 @@ fun main() {
 
     println()
 
-    // ========== TEST WEAPON ==========
     println("========== TEST WEAPON ==========")
     val sword = Weapon("Dragon Sword")
 
-    sword.damage = -50   // Harus ditolak, nilai tetap 0
+    sword.damage = -50
     println("Damage setelah set -50: ${sword.damage}")
 
-    sword.damage = 9999  // Harus jadi 1000
+    sword.damage = 9999
     println("Damage setelah set 9999: ${sword.damage}")
-    println("Tier: ${sword.tier}")  // Legendary
+    println("Tier: ${sword.tier}")
 
     sword.damage = 700
-    println("Tier damage 700: ${sword.tier}")  // Epic
+    println("Tier damage 700: ${sword.tier}")
 
     sword.damage = 300
-    println("Tier damage 300: ${sword.tier}")  // Common
+    println("Tier damage 300: ${sword.tier}")
+
+    println()
+
+    println("========== TEST PLAYER ==========")
+    val player = Player("Andi")
+
+
+    player.addXp(50)
+    println("Level saat ini: ${player.level}")
+
+    player.addXp(60)
+    println("Level saat ini: ${player.level}")
+
+    player.addXp(-10)
+    player.addXp(200)
+    println("Level saat ini: ${player.level}")
 }
