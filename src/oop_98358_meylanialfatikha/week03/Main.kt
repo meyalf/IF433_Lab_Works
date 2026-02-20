02974ec35c2c62bd5d9c735fa852bde9a541ad2f
meyalf <meylanialfatikha@gmail.com>
2026-02-20 15:00:44 +0700

    "week03: test all encapsulation logic in main"

--- src/oop_98358_meylanialfatikha/week03/Main.kt
+++ src/oop_98358_meylanialfatikha/week03/Main.kt
@@ -2,7 +2,17 @@ package oop_98358_meylanialfatikha.week03
 
 fun main() {
     val e = Employee("Budi")
-    e.salary = -1000
+
+    // 1. Test Validasi Salary
+    e.salary = -1000    // Harusnya print Error
     e.salary = 5000000
     println("Gaji: ${e.salary}")
+
+    // 2. Test Encapsulation
+    e.increasePerformance()
+    e.printStatus()
+    // e.performanceRating = 5 // Coba uncomment -> pasti error merah!
+
+    // 3. Test Computed Property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
