1036388dece4fabf6e435c7b3bdf0707dc99b817
meyalf <meylanialfatikha@gmail.com>
2026-02-20 15:06:57 +0700

    "week03: (task 1) implement Weapon testing in main"

--- src/oop_98358_meylanialfatikha/week03/Main.kt
+++ src/oop_98358_meylanialfatikha/week03/Main.kt
@@ -1,6 +1,7 @@
 package oop_98358_meylanialfatikha.week03
 
 fun main() {
+    // ========== TEST EMPLOYEE ==========
     val e = Employee("Budi")
 
     e.salary = -1000
@@ -11,4 +12,23 @@ fun main() {
     e.printStatus()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println()
+
+    // ========== TEST WEAPON ==========
+    println("========== TEST WEAPON ==========")
+    val sword = Weapon("Dragon Sword")
+
+    sword.damage = -50   // Harus ditolak, nilai tetap 0
+    println("Damage setelah set -50: ${sword.damage}")
+
+    sword.damage = 9999  // Harus jadi 1000
+    println("Damage setelah set 9999: ${sword.damage}")
+    println("Tier: ${sword.tier}")  // Legendary
+
+    sword.damage = 700
+    println("Tier damage 700: ${sword.tier}")  // Epic
+
+    sword.damage = 300
+    println("Tier damage 300: ${sword.tier}")  // Common
 }
