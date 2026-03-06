1d4bee086dbdf34cd1ab59ca757669fe5f908878
meyalf <meylanialfatikha@gmail.com>
2026-03-06 15:04:53 +0700

    "week05: create Admin class inheriting Pegawai"

---
+++ src/oop_98358_meylanialfatikha/week05/Admin.kt
@@ -0,0 +1,12 @@
+package oop_98358_meylanialfatikha.week05
+
+class Admin(nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    //Fungsi unik/spesifik yang hanya dimiliki Admin
+    fun doAdminWork(){
+        println("[$nama] sedang merekap data absensi mahasiswa")
+    }
+}
