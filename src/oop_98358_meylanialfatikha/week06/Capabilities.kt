4b6965be192d09d3ff2aee9a87042c4dd10ab320
meyalf <meylanialfatikha@gmail.com>
2026-03-13 15:52:31 +0700

    "week06: create compound interfaces"

---
+++ src/oop_98358_meylanialfatikha/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_98358_meylanialfatikha.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
