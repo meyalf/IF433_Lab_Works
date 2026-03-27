482f41b85289aa63a9de48f3468cc3c282b9ef7d
meyalf <meylanialfatikha@gmail.com>
2026-03-27 14:40:53 +0700

    "week07: test Singleton and Companion Object in main"

--- src/oop_98358_meylanialfatikha/week07/Main.kt
+++ src/oop_98358_meylanialfatikha/week07/Main.kt
@@ -1,6 +1,12 @@
 package oop_98358_meylanialfatikha.week07
 
 fun main() {
-    // ERROR sengaja - private constructor tidak bisa dipanggil langsung
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+    println("Status: ${DatabaseManager.connectionStatus}")
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient() // Instansiasi lewat Factory
+    client.connect()
 }
