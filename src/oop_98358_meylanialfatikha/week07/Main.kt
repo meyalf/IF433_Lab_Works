827edecceb3446e6bf7157a090f8d3e4d4b1ff3d
meyalf <meylanialfatikha@gmail.com>
2026-03-27 14:38:08 +0700

    "week07: (trial) trigger private constructor error on NetworkClient"

---
+++ src/oop_98358_meylanialfatikha/week07/Main.kt
@@ -0,0 +1,6 @@
+package oop_98358_meylanialfatikha.week07
+
+fun main() {
+    // ERROR sengaja - private constructor tidak bisa dipanggil langsung
+    val client = NetworkClient("https://api.umn.ac.id")
+}
