4e4cab5fa3d7fb6b6f7c2d8693d7c6ecd7e1025a
meyalf <meylanialfatikha@gmail.com>
2026-04-17 15:16:12 +0700

    "week08: implement java interop using !!"

--- src/oop_98358_meylanialfatikha/week08/main.kt
+++ src/oop_98358_meylanialfatikha/week08/main.kt
@@ -59,4 +59,9 @@ fun main() {
         println(e.message)
     }
 
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
+
 }
