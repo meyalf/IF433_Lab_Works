47e01d04e1a017254d6e4506354e9b304bf3687a
meyalf <meylanialfatikha@gmail.com>
2026-04-17 15:34:28 +0700

    "week08: (task) create JavaPaymentService for payment interop"

---
+++ src/oop_98358_meylanialfatikha/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_98358_meylanialfatikha.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
