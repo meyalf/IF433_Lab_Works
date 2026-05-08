589dc57ebc4846c069d10873fad2fd7c56f6fcfa
meyalf <meylanialfatikha@gmail.com>
2026-05-08 23:27:35 +0700

    "week11: implement run function for object computation "

--- src/oop_98358_meylanialfatikha/week11/ScopeLetRun.kt
+++ src/oop_98358_meylanialfatikha/week11/ScopeLetRun.kt
@@ -4,10 +4,16 @@ fun main() {
     println("=== TEST LET FUNCTION ===")
     val name: String? = "Alexander"
 
-    // Mengeksekusi blok hanya jika objek tidak null
     val length = name?.let {
-        println("Nama terdeteksi: $it") // 'it' merujuk pada name
-        it.length // Mengembalikan nilai expression terakhir
+        println("Nama terdeteksi: $it")
+        it.length
     }
     println("Panjang nama: $length")
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}")
+        this.length * 2
+    }
+    println("Hasil kalkulasi run: $result")
 }
