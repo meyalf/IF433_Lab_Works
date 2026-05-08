aba0268339d7716fb6dfaf6f073626c77c11b8cb
meyalf <meylanialfatikha@gmail.com>
2026-05-08 23:31:51 +0700

    "week11: combine apply and also in object setup"

--- src/oop_98358_meylanialfatikha/week11/ScopeConfiguration.kt
+++ src/oop_98358_meylanialfatikha/week11/ScopeConfiguration.kt
@@ -17,5 +17,10 @@ fun main() {
     println("\n=== TEST WITH ===")
     with(user) {
         println("User Detail -> Nama: $name, Umur: $age")
+        val newUser = User("Budi", 20).apply {
+            age = 21
+        }.also {
+            println("User baru berhasil dibuat: $it")
+        }
     }
 }
