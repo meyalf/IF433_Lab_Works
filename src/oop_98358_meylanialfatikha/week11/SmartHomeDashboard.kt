717e39460cb3b3bbda2b3576a01380fe48df4563
meyalf <meylanialfatikha@gmail.com>
2026-05-08 23:37:13 +0700

    "week11: (task) implement device search with let"

--- src/oop_98358_meylanialfatikha/week11/SmartHomeDashboard.kt
+++ src/oop_98358_meylanialfatikha/week11/SmartHomeDashboard.kt
@@ -21,4 +21,8 @@ fun main() {
     homeDevices.add(ac)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
