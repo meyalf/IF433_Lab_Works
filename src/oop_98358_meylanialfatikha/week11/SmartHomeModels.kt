bfd040a2658ded3fe3b3784576b0c02e9c7c1285
meyalf <meylanialfatikha@gmail.com>
2026-05-08 23:32:36 +0700

    "week11: (task) create SmartDevice data class"

---
+++ src/oop_98358_meylanialfatikha/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_98358_meylanialfatikha.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
