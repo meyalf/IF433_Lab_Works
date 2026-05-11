ba0f41f95c2622cf36c385f475e0efdc27799659
meyalf <meylanialfatikha@gmail.com>
2026-05-11 11:19:49 +0700

    "week10: test generic functions with Double and String"

--- src/oop_98358_meylanialfatikha/week10/GenericMethods.kt
+++ src/oop_98358_meylanialfatikha/week10/GenericMethods.kt
@@ -2,7 +2,8 @@ package oop_98358_meylanialfatikha.week10
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
-    fun <T> processData(input: T): T {
-        return input
-    }
+}
+
+fun <T> processData(input: T): T {
+    return input
 }
