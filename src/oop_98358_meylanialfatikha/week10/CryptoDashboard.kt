96d63b4fdedec931cb80c4b9a284d9ffb6328951
meyalf <meylanialfatikha@gmail.com>
2026-05-11 11:29:19 +0700

    "week10: (task) wrap repository data into ApiResponse"

--- src/oop_98358_meylanialfatikha/week10/CryptoDashboard.kt
+++ src/oop_98358_meylanialfatikha/week10/CryptoDashboard.kt
@@ -5,4 +5,5 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 2.3))
     coinRepo.add(Coin("USDT", 500.0))
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
