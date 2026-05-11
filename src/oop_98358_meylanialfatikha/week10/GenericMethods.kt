d72844fb395ed684afe9cff9b99f1eaf20df128e
meyalf <meylanialfatikha@gmail.com>
2026-05-11 11:17:39 +0700

    "week10: implement generic function with return type"

--- src/oop_98358_meylanialfatikha/week10/GenericMethods.kt
+++ src/oop_98358_meylanialfatikha/week10/GenericMethods.kt
@@ -2,4 +2,7 @@ package oop_98358_meylanialfatikha.week10
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+    fun <T> processData(input: T): T {
+        return input
+    }
 }
