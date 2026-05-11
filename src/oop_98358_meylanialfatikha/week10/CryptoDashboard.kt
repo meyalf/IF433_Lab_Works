023aad6394018a413acd24ac70bc998ceafc80ec
meyalf <meylanialfatikha@gmail.com>
2026-05-11 11:28:38 +0700

    "week10: (task) populate coin repository"

--- src/oop_98358_meylanialfatikha/week10/CryptoDashboard.kt
+++ src/oop_98358_meylanialfatikha/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_98358_meylanialfatikha.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.3))
+    coinRepo.add(Coin("USDT", 500.0))
 }
