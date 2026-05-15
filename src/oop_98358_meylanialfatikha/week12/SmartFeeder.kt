e535d3abf5fed0f02c74d96193d412fe79d62909
meyalf <meylanialfatikha@gmail.com>
2026-05-15 14:52:45 +0700

    "week12: (task) handle feeding execution with multiple catch"

--- src/oop_98358_meylanialfatikha/week12/SmartFeeder.kt
+++ src/oop_98358_meylanialfatikha/week12/SmartFeeder.kt
@@ -13,4 +13,18 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 }
 fun main() {
     var currentKibbleStock = 50
+    println("=== JADWAL MAKAN PAGI ===")
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException) {
+        println("Error Dispenser: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error Stok: ${e.message}")
+    } catch (e: Exception) {
+        println("Error Umum: ${e.message}")
+    }
 }
