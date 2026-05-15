69f10b00d39c9db83a659156236e5129df673dda
meyalf <meylanialfatikha@gmail.com>
2026-05-15 14:54:38 +0700

    "week12: (task) execute feeding using runCatching"

--- src/oop_98358_meylanialfatikha/week12/SmartFeeder.kt
+++ src/oop_98358_meylanialfatikha/week12/SmartFeeder.kt
@@ -30,4 +30,12 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    println("\n=== JADWAL MAKAN SORE ===")
+    runCatching {
+        dispenseKibble(
+            requestedGram = 30,
+            availableGram = 1000,
+            isJammed = false
+        )
+    }
 }
