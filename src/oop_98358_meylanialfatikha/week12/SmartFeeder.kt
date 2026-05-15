5eb9658588036db1592143a73437a9fdeb70a633
meyalf <meylanialfatikha@gmail.com>
2026-05-15 14:49:05 +0700

    "week12: (task) implement dispenseKibble function"

---
+++ src/oop_98358_meylanialfatikha/week12/SmartFeeder.kt
@@ -0,0 +1,6 @@
+package oop_98358_meylanialfatikha.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    return 0
+}
